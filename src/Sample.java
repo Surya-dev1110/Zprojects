1ff4734081d1fa46f01cb2a075f50923869c06c6
Surya <suryadevaraj1110@gmail.com>
2024-07-09 17:09:49 +0530

    sample commit

--- src/Sample.java
+++ src/Sample.java
@@ -1,8 +1,13 @@
+
+
 public class Sample
 {
     public static void main(String[] args)
     {
         System.out.println("Hii");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
 
